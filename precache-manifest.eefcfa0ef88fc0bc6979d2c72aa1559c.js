c71f50061b30d8276c6c594a10753219cdbeaed1
BaghdasaryanNairi <nairi.baghdasaryan.github.io>
2019-04-14 22:15:12 +0400

    Updates

---
+++ precache-manifest.eefcfa0ef88fc0bc6979d2c72aa1559c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "231fb89e210e3b1d7106",
+    "url": "/Nairibaghdasaryan.github.io/static/css/main.ed22d6ef.chunk.css"
+  },
+  {
+    "revision": "231fb89e210e3b1d7106",
+    "url": "/Nairibaghdasaryan.github.io/static/js/main.e25088c3.chunk.js"
+  },
+  {
+    "revision": "66bc8e7f296f0b7532d1",
+    "url": "/Nairibaghdasaryan.github.io/static/js/runtime~main.28033498.js"
+  },
+  {
+    "revision": "44ecb944593428d29544",
+    "url": "/Nairibaghdasaryan.github.io/static/css/2.4f049491.chunk.css"
+  },
+  {
+    "revision": "44ecb944593428d29544",
+    "url": "/Nairibaghdasaryan.github.io/static/js/2.7c1c9f87.chunk.js"
+  },
+  {
+    "revision": "1905ffb8a875059e9fa178f88f90e23b",
+    "url": "/Nairibaghdasaryan.github.io/index.html"
+  }
+];
